fd3d34b347a94b9c9d701dd03a76c0172f416104
vish9568 <65942521+vish9568@users.noreply.github.com>
2020-11-27 09:59:27 +0530

    Add files via upload

---
+++ Day 5/que1.py
@@ -0,0 +1,15 @@
+def myFunction(x,y):
+	print("Addition of two numbers:")
+	print(x+y)
+	print("Substraction of two numbers:")
+	print(x-y)
+	print("Division of two numbers:")
+	print(x/y)
+	print("Multilication of two numbers:")
+	print(x*y)
+
+
+
+x=int(input("Enter first number:"))
+y=int(input("Enter second number:"))
+myFunction(x,y)
---
+++ Day 5/que2.py
@@ -0,0 +1,7 @@
+def covid(name,body_temp):
+	print(name)
+	print(body_temp)
+
+name=input()
+body_temp=98
+covid(name,body_temp)	
